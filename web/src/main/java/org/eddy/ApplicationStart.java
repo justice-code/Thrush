bf01ee7c970007eba47d1ccc2c00fad0d2477f3b
Justice-love <eddyxu1213@126.com>
2017-07-13 17:11:40 +0800

    增加spring boot 启动

---
+++ web/src/main/java/org/eddy/ApplicationStart.java
@@ -0,0 +1,20 @@
+package org.eddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+
+/**
+ * Created by Justice-love on 2017/7/13.
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ApplicationStart {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(ApplicationStart.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("thrush.pid"));
+        springApplication.run(args);
+    }
+}

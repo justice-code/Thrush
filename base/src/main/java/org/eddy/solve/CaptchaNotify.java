7bfcf840f412f7619c90a55f6fa621037e9cde62
Justice-love <eddyxu1213@126.com>
2017-07-16 11:02:25 +0800

    图片坐标计算

---
+++ base/src/main/java/org/eddy/solve/CaptchaNotify.java
@@ -0,0 +1,15 @@
+package org.eddy.solve;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Justice-love on 2017/7/16.
+ */
+@Getter @Setter
+public class CaptchaNotify {
+
+    private String pipeline;
+
+    private String[] number;
+}
---
+++ solve/src/main/java/org/eddy/pipeline/CoordinateUtil.java
@@ -0,0 +1,8 @@
+package org.eddy.pipeline;
+
+/**
+ * Created by Justice-love on 2017/7/16.
+ */
+public class CoordinateUtil {
+
+}

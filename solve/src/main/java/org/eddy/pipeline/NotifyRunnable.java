526169db4f7fc58336deb592327c9fdffc2b7126
Justice-love <eddyxu1213@126.com>
2017-07-17 17:30:47 +0800

    执行流程

--- solve/src/main/java/org/eddy/pipeline/NotifyRunnable.java
+++ solve/src/main/java/org/eddy/pipeline/NotifyRunnable.java
@@ -1,10 +1,13 @@
 package org.eddy.pipeline;
 
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 import org.eddy.solve.CaptchaNotify;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
 
 /**
  * Created by Justice-love on 2017/7/17.
@@ -17,7 +20,7 @@ public class NotifyRunnable implements Runnable {
     private static final int DEFAULT_TIME = 3;
 
     @NonNull
-    private String notifyGroup;
+    private String pipelineGroup;
 
     @Autowired
     private Pipeline pipeline;
@@ -25,5 +28,26 @@ public class NotifyRunnable implements Runnable {
     @Override
     public void run() {
         CaptchaNotify notify = pipeline.pollNotify(DEFAULT_TIME);
+        Objects.requireNonNull(notify);
+        if (! StringUtils.equals(notify.getPipeline(), this.getPipelineGroup())) {
+
+        }
+    }
+
+    private CaptchaNotify findNotify() {
+        CaptchaNotify notify = pipeline.pollNotify(DEFAULT_TIME);
+        Objects.requireNonNull(notify);
+
+        if (StringUtils.equals(notify.getPipeline(), this.getPipelineGroup())) {
+            return notify;
+        }
+
+        try {
+            pipeline.putNotify(notify);
+            Thread.sleep(300);
+            return findNotify();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("findNotify error", e);
+        }
     }
 }

5ae94f37b61461cca3242808103589e7768e699a
Justice-love <eddyxu1213@126.com>
2017-07-21 09:16:10 +0800

    增加钉钉机器人反馈

---
+++ base/src/main/java/org/eddy/config/DingConfig.java
@@ -0,0 +1,24 @@
+package org.eddy.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Justice-love on 2017/7/21.
+ */
+@ConfigurationProperties("thrush.ding")
+@Component
+public class DingConfig {
+
+    public static String url;
+
+    public static String token;
+
+    public void setUrl(String url) {
+        DingConfig.url = url;
+    }
+
+    public void setToken(String token) {
+        DingConfig.token = token;
+    }
+}

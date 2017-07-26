22804b8bb5cde1506b4d49d878e2930760052c91
Justice-love <eddyxu1213@126.com>
2017-07-26 11:13:47 +0800

    增加车次选择入口

--- web/src/main/java/org/eddy/controller/WebController.java
+++ web/src/main/java/org/eddy/controller/WebController.java
@@ -1,10 +1,17 @@
 package org.eddy.controller;
 
+import org.eddy.pipeline.Pipeline;
+import org.eddy.pipeline.command.Command;
+import org.eddy.pipeline.command.CommandNotify;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Objects;
 
 /**
  * Created by Justice-love on 2017/7/24.
@@ -17,5 +24,18 @@ public class WebController {
     public String captcha(@RequestParam("pipeline") String pipeline, Model model) {
         model.addAttribute("pipeline", pipeline);
         return "login/captcha";
+    }
+
+    @RequestMapping(value = "/tranBegin", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Void> tranBegin(@RequestParam("pipeline") String pipeline) {
+        Objects.requireNonNull(pipeline);
+
+        CommandNotify notify = new CommandNotify();
+        notify.setPipeline(pipeline);
+        notify.setCommand(Command.START_CHOOSE_TRAIN);
+
+        Pipeline.putNotify(notify);
+        return ResponseEntity.ok().build();
     }
 }

132d78837b4d01989d37b793f4e51cad611dc761
Justice-love <eddyxu1213@126.com>
2017-09-14 11:14:16 +0800

    增加登录控制器和页面

---
+++ web/src/main/java/org/eddy/controller/LoginController.java
@@ -0,0 +1,32 @@
+package org.eddy.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eddy.aop.LoginCheckAspect;
+import org.eddy.config.LoginUserMessageConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private LoginUserMessageConfig loginConfig;
+
+    @RequestMapping(value = "/adminLogin", method = RequestMethod.POST)
+    public String login(HttpSession session, String password) {
+        if (StringUtils.equals(password, loginConfig.getPassword())) {
+            session.setAttribute(LoginCheckAspect.LOGIN_ATTR_KEY, true);
+        }
+        return "redirect:/page/";
+    }
+
+    @RequestMapping("/toLogin")
+    public String login() {
+        return "login/login";
+    }
+}
